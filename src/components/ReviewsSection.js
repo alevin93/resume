a03773f2e572019d074a5e58336f4fb571932d1c
Alex Levin <alex.levin93@gmail.com>
2022-08-19 18:11:57 -0700

    changes

--- src/components/ReviewsSection.js
+++ src/components/ReviewsSection.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import styled from 'styled-components';
 import { InnerLayout } from '../styles/Layouts';
-import ReviewItem from './ReviewItem';
 //import { InnerLayout } from '../styles/Layouts'
 import Title from './Title';
 
